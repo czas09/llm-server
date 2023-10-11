f3662ff91cc461ae3e68f5c78ef3dbbe73e0c6ce
zhuoyiwei <zhuoyiwei@fhxk.com>
2023-10-12 04:26:52 +0800

    完善服务配置参数设置

--- config.py
+++ config.py
@@ -69,48 +69,48 @@ class Config:
         # 服务配置选项
         # ==============================================================================
         self.SERVICE_HOST = config.get("SERVICE", "host") \
-            if config.get("SERVICE", "host") is not "" else DEFAULT_CONFIGS["SERVICE_HOST"]
+            if config.get("SERVICE", "host") != "" else DEFAULT_CONFIGS["SERVICE_HOST"]
         self.SERVICE_PORT = config.get("SERVICE", "port") \
-            if config.get("SERVICE", "port") is not "" else DEFAULT_CONFIGS["SERVICE_PORT"]
+            if config.get("SERVICE", "port") != "" else DEFAULT_CONFIGS["SERVICE_PORT"]
         self.API_PREFIX = config.get("SERVICE", "prefix") \
-            if config.get("SERVICE", "prefix") is not "" else DEFAULT_CONFIGS["API_PREFIX"]
+            if config.get("SERVICE", "prefix") != "" else DEFAULT_CONFIGS["API_PREFIX"]
         self.API_KEYS = config.get("SERVICE", "api_keys") \
-            if config.get("SERVICE", "api_keys") is not "" else DEFAULT_CONFIGS["API_KEYS"]
+            if config.get("SERVICE", "api_keys") != "" else DEFAULT_CONFIGS["API_KEYS"]
 
         # ==============================================================================
         # 模型配置选项
         # ==============================================================================
         self.MODEL_NAME = config.get("MODEL", "model_name").lower() \
-            if config.get("MODEL", "model_name") is not "" else DEFAULT_CONFIGS["MODEL_NAME"]
+            if config.get("MODEL", "model_name") != "" else DEFAULT_CONFIGS["MODEL_NAME"]
         self.MODEL_PATH = config.get("MODEL", "model_path") \
-            if config.get("MODEL", "model_path") is not "" else DEFAULT_CONFIGS["MODEL_PATH"]
+            if config.get("MODEL", "model_path") != "" else DEFAULT_CONFIGS["MODEL_PATH"]
         self.ADAPTER_PATH = config.get("MODEL", "adapter_path") \
-            if config.get("MODEL", "adapter_path") is not "" else DEFAULT_CONFIGS["ADAPTER_PATH"]
+            if config.get("MODEL", "adapter_path") != "" else DEFAULT_CONFIGS["ADAPTER_PATH"]
         self.RESIZE_EMBEDDING = config.get("MODEL", "resize_embedding") \
-            if config.get("MODEL", "resize_embedding") is not "" else DEFAULT_CONFIGS["RESIZE_EMBEDDING"]
+            if config.get("MODEL", "resize_embedding") != "" else DEFAULT_CONFIGS["RESIZE_EMBEDDING"]
 
         self.DEVICE = config.get("MODEL", "device") \
-            if config.get("MODEL", "device") is not "" else DEFAULT_CONFIGS["DEVICE"]
+            if config.get("MODEL", "device") != "" else DEFAULT_CONFIGS["DEVICE"]
         self.DEVICE_MAP = config.get("MODEL", "device_map") \
-            if config.get("MODEL", "device_map") is not "" else DEFAULT_CONFIGS["DEVICE_MAP"]
+            if config.get("MODEL", "device_map") != "" else DEFAULT_CONFIGS["DEVICE_MAP"]
         self.GPUS = config.get("MODEL", "gpus") \
-            if config.get("MODEL", "gpus") is not "" else DEFAULT_CONFIGS["GPUS"]
+            if config.get("MODEL", "gpus") != "" else DEFAULT_CONFIGS["GPUS"]
         self.NUM_GPUS = config.getint("MODEL", "num_gpus") \
-            if config.getint("MODEL", "num_gpus") is not "" else DEFAULT_CONFIGS["NUM_GPUS"]
+            if config.getint("MODEL", "num_gpus") != "" else DEFAULT_CONFIGS["NUM_GPUS"]
 
         self.QUANTIZE = config.get("MODEL", "quantize") \
-            if config.get("MODEL", "quantize") is not "" else DEFAULT_CONFIGS["QUANTIZE"]
+            if config.get("MODEL", "quantize") != "" else DEFAULT_CONFIGS["QUANTIZE"]
         self.LOAD_IN_8BIT = config.get("MODEL", "load_in_8bit") \
-            if config.get("MODEL", "load_in_8bit") is not "" else DEFAULT_CONFIGS["LOAD_IN_8BIT"]
+            if config.get("MODEL", "load_in_8bit") != "" else DEFAULT_CONFIGS["LOAD_IN_8BIT"]
         self.LOAD_IN_4BIT = config.get("MODEL", "load_in_4bit") \
-            if config.get("MODEL", "load_in_4bit") is not "" else DEFAULT_CONFIGS["LOAD_IN_4BIT"]
+            if config.get("MODEL", "load_in_4bit") != "" else DEFAULT_CONFIGS["LOAD_IN_4BIT"]
         self.USING_PTUNING_V2 = config.get("MODEL", "using_ptuning_v2") \
-            if config.get("MODEL", "using_ptuning_v2") is not "" else DEFAULT_CONFIGS["USING_PTUNING_V2"]
+            if config.get("MODEL", "using_ptuning_v2") != "" else DEFAULT_CONFIGS["USING_PTUNING_V2"]
 
         self.CONTEXT_LEN = config.get("MODEL", "context_len") \
-            if config.get("MODEL", "context_len") is not "" else DEFAULT_CONFIGS["CONTEXT_LEN"]
+            if config.get("MODEL", "context_len") != "" else DEFAULT_CONFIGS["CONTEXT_LEN"]
         self.STREAM_INTERVERL = config.get("MODEL", "stream_interval") \
-            if config.get("MODEL", "stream_interval") is not "" else DEFAULT_CONFIGS["STREAM_INTERVERL"]
+            if config.get("MODEL", "stream_interval") != "" else DEFAULT_CONFIGS["STREAM_INTERVERL"]
         # self.PROMPT_NAME = config_parser.get("MODEL", "")    # TODO(@zyw)
 
         # ==============================================================================
@@ -118,29 +118,29 @@ class Config:
         # ==============================================================================
         # self.USE_VLLM = 
         self.SERVING_ENGINE = config.get("SERVING", "serving_engine") \
-            if config.get("SERVING", "serving_engine") is not "" else DEFAULT_CONFIGS["SERVING_ENGINE"]
+            if config.get("SERVING", "serving_engine") != "" else DEFAULT_CONFIGS["SERVING_ENGINE"]
 
         # transformers 相关配置项
         self.USE_STREAMER_V2 = config.get("SERVING", "use_streamer_v2") \
-            if config.get("SERVING", "use_streamer_v2") is not "" else DEFAULT_CONFIGS["USE_STREAMER_V2"]
+            if config.get("SERVING", "use_streamer_v2") != "" else DEFAULT_CONFIGS["USE_STREAMER_V2"]
 
         # vLLM 相关配置项
         self.TRUST_REMOTE_CODE = config.get("SERVING", "trust_remote_code") \
-            if config.get("SERVING", "trust_remote_code") is not "" else DEFAULT_CONFIGS["TRUST_REMOTE_CODE"]
+            if config.get("SERVING", "trust_remote_code") != "" else DEFAULT_CONFIGS["TRUST_REMOTE_CODE"]
         self.TOKENIZE_MODE = config.get("SERVING", "tokenize_mode") \
-            if config.get("SERVING", "tokenize_mode") is not "" else DEFAULT_CONFIGS["TOKENIZE_MODE"]
+            if config.get("SERVING", "tokenize_mode") != "" else DEFAULT_CONFIGS["TOKENIZE_MODE"]
         self.TENSOR_PARALLEL_SIZE = config.get("SERVING", "tensor_parallel_size") \
-            if config.get("SERVING", "tensor_parallel_size") is not "" else DEFAULT_CONFIGS["TENSOR_PARALLEL_SIZE"]
+            if config.get("SERVING", "tensor_parallel_size") != "" else DEFAULT_CONFIGS["TENSOR_PARALLEL_SIZE"]
         self.DTYPE = config.get("SERVING", "dtype") \
-            if config.get("SERVING", "dtype") is not "" else DEFAULT_CONFIGS["DTYPE"]
+            if config.get("SERVING", "dtype") != "" else DEFAULT_CONFIGS["DTYPE"]
         self.GPU_MEMORY_UTILIZATION = config.get("SERVING", "gpu_memory_utilization") \
-            if config.get("SERVING", "gpu_memory_utilization") is not "" else DEFAULT_CONFIGS["GPU_MEMORY_UTILIZATION"]
+            if config.get("SERVING", "gpu_memory_utilization") != "" else DEFAULT_CONFIGS["GPU_MEMORY_UTILIZATION"]
         self.MAX_NUM_BATCHED_TOKENS = config.get("SERVING", "max_num_batched_tokens") \
-            if config.get("SERVING", "max_num_batched_tokens") is not "" else DEFAULT_CONFIGS["MAX_NUM_BATCHED_TOKENS"]
+            if config.get("SERVING", "max_num_batched_tokens") != "" else DEFAULT_CONFIGS["MAX_NUM_BATCHED_TOKENS"]
         self.MAX_NUM_SEQS = config.get("SERVING", "max_num_seqs") \
-            if config.get("SERVING", "max_num_seqs") is not "" else DEFAULT_CONFIGS["MAX_NUM_SEQS"]
+            if config.get("SERVING", "max_num_seqs") != "" else DEFAULT_CONFIGS["MAX_NUM_SEQS"]
         self.QUANTIZATION_METHOD = config.get("SERVING", "quantization_method") \
-            if config.get("SERVING", "quantization_method") is not "" else DEFAULT_CONFIGS["QUANTIZATION_METHOD"]
+            if config.get("SERVING", "quantization_method") != "" else DEFAULT_CONFIGS["QUANTIZATION_METHOD"]
 
         # LMDeploy 相关配置项
         # TODO(@zyw)
