6651999464f14ee3316fe8f8697a31f3378f2dcd
zhuoyiwei <zhuoyiwei@fhxk.com>
2023-10-21 21:51:19 +0800

    为接口参数添加注释

--- protocol.py
+++ protocol.py
@@ -6,10 +6,11 @@ from typing import Literal, Optional, List, Dict, Any, Union
 from pydantic import BaseModel, Field
 
 
-class Role(str, Enum):
-    USER = "user"
-    ASSISTANT = "assistant"
-    SYSTEM = "system"
+class Role(str, Enum): 
+    """对话角色"""
+    SYSTEM = "system"          # 系统提示，用户提供，类似 instruction
+    USER = "user"              # 用户输入问题
+    ASSISTANT = "assistant"    # 对话模型回复
     # FUNCTION = "function"
 
 
@@ -76,27 +77,27 @@ class ChatMessage(BaseModel):
 
 
 class ChatCompletionRequest(BaseModel):
-    model: str
-    messages: Optional[List[ChatMessage]]
-    temperature: Optional[float] = 0.7
-    top_p: Optional[float] = 1.0
-    n: Optional[int] = 1
+    model: str                                      # 模型名称
+    messages: Optional[List[ChatMessage]]           # 输入文本（可包含对话历史）
+    temperature: Optional[float] = 0.7              # 温度，用于控制生成文本的多样性或风格
+    top_p: Optional[float] = 1.0                    # 用于控制模型生成下一个 token 时，在概率分布中的选择范围
+    n: Optional[int] = 1                            # 返回候选文本数量
     max_tokens: Optional[int] = None
-    stop: Optional[Union[str, List[str]]] = None
-    stream: Optional[bool] = False
-    presence_penalty: Optional[float] = 0.0
-    frequency_penalty: Optional[float] = 0.0
+    stop: Optional[Union[str, List[str]]] = None    # 停止词，文本生成过程在遇到这里指定的词汇后被截停
+    stream: Optional[bool] = False                  # 是否流式传输
+    presence_penalty: Optional[float] = 0.0         # 用于控制模型是否生成已生成的词汇：0 表示更有可能生成重复内容，1 表示生成完全不重复的内容
+    frequency_penalty: Optional[float] = 0.0        # 用于控制模型是否生成常见词汇：0 表示更有可能生成常见词汇，1 表示完全避免生成常见词汇
     user: Optional[str] = None
-    # functions: Optional[List[ChatFunction]] = None
+    # functions: Optional[List[ChatFunction]] = None    # TODO(@zyw): 暂不支持 function call 相关功能
     # function_call: Union[str, Dict[str, str]] = "auto"
 
     # Additional parameters support for stop generation
     stop_token_ids: Optional[List[int]] = None
-    repetition_penalty: Optional[float] = 1.1
+    repetition_penalty: Optional[float] = 1.1       # 作用与 stop 类似，但这里指定的是 token_ids
 
     # Additional parameters supported by vLLM
     best_of: Optional[int] = None
-    top_k: Optional[int] = -1
+    top_k: Optional[int] = -1                       # 作用与 top_p 类似，但限定的是 token 在概率分布中从大到小排名的前 k 个
     ignore_eos: Optional[bool] = False
     use_beam_search: Optional[bool] = False
     skip_special_tokens: Optional[bool] = True
