cb509161341b6edb84657faa54137e72c932585a
zhuoyiwei <zhuoyiwei@fhxk.com>
2023-11-11 03:52:17 +0800

    tiny update

--- configs/model_paths.py
+++ configs/model_paths.py
@@ -2,33 +2,36 @@
 CHAT_MODEL_ROOTS = [
     "/IAOdata/models",      # 在宿主机上的路径
     "/workspace/models",    # 在 Docker 镜像中的路径
+    # TODO FHLLM 路径
 ]
 
 # 对话模型文件版本
 CHAT_MODEL_NAME_MAP = {
-    "chatglm-6b": "chatglm-6b-20230515", 
-    "chatglm2-6b": "chatglm2-6b-20230625", 
-    "baichuan-13b-chat": "baichuan-13b-chat-20230718",    # 20230801
-    "baichuan2-7b-chat": "baichuan2-7b-chat-20230906", 
-    "baichuan2-13b-chat": "baichuan2-13b-chat-20230906", 
-    "qwen-7b-chat": None, 
-    "qwen-14b-chat": "qwen-14b-chat-20230924", 
-    "internlm-chat-7b-v1-1": "internlm-chat-7b-v1-1-20230901",    # 20230822, 20230907
-    "internlm-chat-20b": "internlm-chat-20b-20230920", 
-    "xverse-13b-chat": "xverse-13b-chat-20230819", 
     "aquilachat2-7b": "aquilachat2-7b-20231011", 
     "aquilachat2-34b": "aquilachat2-34b-20231011", 
+    "baichuan-13b-chat": "baichuan-13b-chat-20230718",            # 20230801
+    "baichuan2-7b-chat": "baichuan2-7b-chat-20230906", 
+    "baichuan2-13b-chat": "baichuan2-13b-chat-20230906", 
+    "chatglm-6b": "chatglm-6b-20230515", 
+    "chatglm2-6b": "chatglm2-6b-20230625", 
+    "chatglm3-6b": "chatglm3-6b-20231026", 
+    "internlm-chat-7b-v1-1": "internlm-chat-7b-v1-1-20230901",    # 20230822, 20230907
+    "internlm-chat-20b": "internlm-chat-20b-20230920", 
+    "qwen-7b-chat": "qwen-7b-chat-20230924",                      # 20230803
+    "qwen-14b-chat": "qwen-14b-chat-20230924", 
+    "xverse-13b-chat": "xverse-13b-chat-20230819", 
 }
 
 # TODO(@zyw): 模型最大容量
 CHAT_MODEL_MAX_LEN_MAP = {
     "chatglm-6b": 2048, 
     "chatglm2-6b": 8192, 
+    "chatglm3-6b": 8192, 
     "baichuan-13b-chat": 4096, 
     "baichuan2-7b-chat": 4096, 
     "baichuan2-13b-chat": 4096, 
-    "qwen-7b-chat": 2048, 
-    "qwen-14b-chat": 2048, 
+    "qwen-7b-chat": 8192,       # 202308 旧版为 2048
+    "qwen-14b-chat": 8192, 
     "internlm-chat-7b-v1-1": 2048, 
     "internlm-chat-20b": 2048, 
     "xverse-13b-chat": 2048, 
--- llms/__init__.py
+++ llms/__init__.py
@@ -1,6 +1,10 @@
 from .base import BaseChatModel, BaseModelAdapter, BasePromptAdapter
-# from .chatglm import ChatGLM
-from .chatglm2 import ChatGLM2
+# from .aquila import Aquila
 from .baichuan import Baichuan
 from .baichuan2 import Baichuan2
+# from .chatglm import ChatGLM
+from .chatglm2 import ChatGLM2
+from .chatglm3 import ChatGLM3
 from .internlm import InternLM
+from .qwen import Qwen
+# from .xverse import XVERSE
